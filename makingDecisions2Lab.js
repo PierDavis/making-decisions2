f63d2f49a39beba930a6e169ffe6a481a414fadb
Pier Davis <pieramelia2@gmail.com>
2021-10-15 15:24:17 -0400

    friday edits

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -121,13 +121,15 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Do not edit the code above.
 
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
-
+let divisibleByThree= []
 //Code Here
 for (let i = 0; i < listOfNumbers.length; i++){
-  if (listOfNumbers[i] % 3 ===0)
-  console.log("[i] is divisible by 3.")
+  if (listOfNumbers[i] % 3 ===0){
+ divisibleByThree.push(listOfNumbers[i])
+    console.log(listOfNumbers[i],"is divisible by 3.")
 }
-
+}
+console.log("divisibleByThree", divisibleByThree)
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
